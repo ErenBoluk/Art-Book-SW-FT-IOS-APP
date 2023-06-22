c4010aeb94ce19fbf862cee6a2bcc855e8041a6b
ErenbolukGappze <erenboluk@hotmail.com>
2023-06-22 21:58:58 +0300

    Delete Operation Added

--- ArtBookProject/ArtBookProject/detailVC.swift
+++ ArtBookProject/ArtBookProject/detailVC.swift
@@ -16,18 +16,56 @@ class detailVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     @IBOutlet weak var artistText: UITextField!
     @IBOutlet weak var yearText: UITextField!
     
+    @IBOutlet weak var saveButton: UIButton!
     var chosenPainting = ""
     var chosenPaintingId : UUID?
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if chosenPainting != ""	{
-            // coredara
-            let stringUUID = chosenPaintingId?.uuidString
-            print(stringUUID)
+            saveButton.isHidden = true
+            // core data
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
             
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count>0 {
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "name") as? String{
+                            nameText.text = name
+                        }
+                        
+                        if let artist = result.value(forKey: "artist") as? String{
+                            artistText.text = artist
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int{
+                            yearText.text = String(year)
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data{
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                        
+                    }
+                }
+            }catch{
+                print("Error")
+            }
+            
+    
         }else{
-            
+            saveButton.isHidden = false
+            saveButton.isEnabled = false
+            nameText.text = ""
+            artistText.text = ""
+            yearText.text = ""
         }
         
 //        Recognizers
@@ -49,6 +87,7 @@ class detailVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
+        saveButton.isEnabled = true
         self.dismiss(animated: true, completion: nil)
     }
     
